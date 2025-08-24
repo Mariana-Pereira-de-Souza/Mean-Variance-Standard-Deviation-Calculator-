5a1b3d9ddee976085ddbc20ed6b420c3dae3eaa4
Mariana Pereira <mariana.pereira@sou.inteli.edu.br>
2025-08-24 23:29:14 +0000

    Aterações no código

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,19 +1,18 @@
 import numpy as np
 
 def calculate(list):
-    # 1. Verificar se tem exatamente 9 números
+   
     if len(list) != 9:
        raise ValueError("List must contain nine numbers.")
 
-    # 2. Transformar em matriz 3x3
+    
     matriz = np.array(list).reshape(3, 3)
 
-    # 3. Calcular estatísticas
     calculations = {
         'mean': [
-            matriz.mean(axis=0).tolist(),   # média por coluna
-            matriz.mean(axis=1).tolist(),   # média por linha
-            matriz.mean().tolist()          # média total
+            matriz.mean(axis=0).tolist(),   
+            matriz.mean(axis=1).tolist(),  
+            matriz.mean().tolist()          
         ],
         'variance': [
             matriz.var(axis=0).tolist(),
